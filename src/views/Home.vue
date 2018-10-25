09a3a4292a70c5616d253d6c253bb055d2d898e1
hfj <fujian.hou@bestmath.cn>
2018-10-25 13:53:39 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Row>
+      <Col>人头价格：<InputNumber :max="10"
+                   :min="0.1"
+                   v-model="baseNumber"
+                   :step="0.1"></InputNumber>
+      </Col>
+
+    </Row>
+
+    <Divider></Divider>
+    <Row>
+      <Col span="2">大飞：</Col>
+      <Col span="2">
+      <InputNumber v-model="fly"
+                   placeholder="输入人头" />
+      </Col>
+      <Col span="2">￥：<span>{{this.baseNumber * fly}}</span> </Col>
+    </Row>
+    <Divider></Divider>
+    <Row>
+      <Col span="2">豆子：</Col>
+      <Col span="2">
+      <InputNumber v-model="bean"
+                   placeholder="输入人头" />
+      </Col>
+      <Col span="2">￥：<span>{{this.baseNumber * bean}}</span> </Col>
+    </Row>
+    <Divider></Divider>
+    <Row>
+      <Col span="2">鹏哥：</Col>
+      <Col span="2">
+      <InputNumber v-model="bird"
+                   placeholder="输入人头" />
+      </Col>
+      <Col span="2">￥：<span>{{this.baseNumber * bird}}</span> </Col>
+    </Row>
+    <Divider></Divider>
+    <Row>
+      <Col span="2">侯哥：</Col>
+      <Col span="2">
+      <InputNumber v-model="monkey"
+                   placeholder="输入人头" />
+      </Col>
+      <Col span="2">￥：<span>{{this.baseNumber * monkey}}</span> </Col>
+    </Row>
   </div>
 </template>
 
@@ -11,8 +54,32 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      fly: "",
+      bean: "",
+      bird: "",
+      monkey: "",
+      baseNumber: 0.5
+    };
+  },
+
+  computed: {
+    countingDafei() {
+      return this.baseNumber * fly;
+    }
+  },
+
   components: {
     HelloWorld
   }
 };
 </script>
+
+<style>
+.title {
+  font-size: 40px;
+  color: red;
+}
+</style>
+
