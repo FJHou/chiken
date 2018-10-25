92aadd5c789cb2bdb8745adeedae45973338ff79
hfj <fujian.hou@bestmath.cn>
2018-10-25 14:04:52 +0800

    build

---
+++ dist/precache-manifest.28d024c998f886b00f39cf9afee26755.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "96485c189746db42165e",
+    "url": "/js/chunk-vendors.1d0eecca.js"
+  },
+  {
+    "revision": "febd5e69dd244befd972",
+    "url": "/js/app.9e451321.js"
+  },
+  {
+    "revision": "65b0b2e345b9bdc60687",
+    "url": "/js/about.e8de88ed.js"
+  },
+  {
+    "revision": "360e2047f912308bca97f01d910ce66b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "96485c189746db42165e",
+    "url": "/css/chunk-vendors.2c602adb.css"
+  },
+  {
+    "revision": "febd5e69dd244befd972",
+    "url": "/css/app.48653676.css"
+  }
+];
